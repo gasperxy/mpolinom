bf3fe4d51630f6a96d759f87a60053d763f158ab
SaraBrezec <sara.brezec@gmail.com>
2020-05-05 19:28:23 +0200

    add search index function

--- search/views.py
+++ search/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
+from search.documents import MpolynomDocument
 
 # Create your views here.
 from django.http import HttpResponse
 
-# popravi na novo razdeljene applikacije
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the search index.")
-def about(request):
-    return HttpResponse("This site is about M-polynomials page.")
-def instructions(request):
-    return HttpResponse("Here are usage instructions and hints." )
-def contribute(request):
-    return HttpResponse("Contribute new M-polynomials or comment.")
-def register(request):
-    return HttpResponse("Registring allows you to contribute new M-polynomials and comment.")
-def advanced_search(request):
-    return HttpResponse("Advanced search.")
+    q = request.GET.get('q')
+    if q:
+        results = MpolynomDocument.search().query("match", structure_name = q)
+    else:
+        results= "Ni ujemanja"
+    return render(request, 'search/index.html', {'results': results})
+def results(request):
+    return HttpResponse("Search results or error.")
+# def about(request):
+#     return HttpResponse("This site is about M-polynomials page.")
+# def instructions(request):
+#     return HttpResponse("Here are usage instructions and hints." )
+# def contribute(request):
+#     return HttpResponse("Contribute new M-polynomials or comment.")
+# def register(request):
+#     return HttpResponse("Registring allows you to contribute new M-polynomials and comment.")
+# def advanced_search(request):
+#     return HttpResponse("Advanced search.")
