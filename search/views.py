882bbc097554d57d420549c55273030ce5925a74
SaraBrezec <sara.brezec@gmail.com>
2020-05-06 19:06:32 +0200

    add elasticsearch query

--- search/views.py
+++ search/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
 from search.documents import MpolynomDocument
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search, Q
+
+client = Elasticsearch()
+s = Search(using=client)
 
 # Create your views here.
 from django.http import HttpResponse
 
 
-def index(request):
+def index(request): #search all fields
     q = request.GET.get('q')
     if q:
-        results = MpolynomDocument.search().query("match", structure_name = q)
+        results =  MpolynomDocument.search().query("multi_match", query = q, fields = ['mpolynomyal^3',
+        'structure_name^3','keywords^2','comments','references','links','author^2'],fuzziness = 0) 
     else:
         results= "Ni ujemanja"
     return render(request, 'search/index.html', {'results': results})
-def results(request):
-    return HttpResponse("Search results or error.")
 # def about(request):
 #     return HttpResponse("This site is about M-polynomials page.")
 # def instructions(request):
