eac9fc135ad0bf51577686eb6cc2ee7b61b96905
SaraBrezec <sara.brezec@gmail.com>
2020-05-29 16:19:51 +0200

    tune parameters for search

--- search/views.py
+++ search/views.py
@@ -11,15 +11,27 @@ from .models import rewrite_mpolynomial
 # Create your views here.
 from django.http import HttpResponse
 
+def mpoly_query(query, lte, gte):
+    q0 = Q('bool',
+        must=[Q("multi_match", query = query, fields = ['mpolynomyal^4','structure_name^3','keywords^2',
+        'comments','references','links','author^2'],fuzziness = "0", minimum_should_match = '-10%'# 90, '85%' 
+        ),
+        Q('range',  nb_tokens = {'lte': lte, 'gte': gte})
+    ]) 
+    results0 = MpolynomDocument.search().query(q0) 
+    response0 = results0.execute()
+    return response0
+# "AUTO:4,7"
+
 
 def index(request): #search all fields
     q = request.GET.get('q')
     if q:
         # remove whitespace and check if mpolynom
+        #     sign_list.append("^"+ str(i))
         wq = q.replace(" ", "")
         sign_list = ["x^", "y^"]
         # for i in range(2,10):
-        #     sign_list.append("^"+ str(i))
         for sign in sign_list:
             # contains mpoly sign
             if wq.find(sign) != -1:
@@ -27,53 +39,27 @@ def index(request): #search all fields
                 b = rewrite_mpolynomial(wq)
                 split = b.split()
                 nb_tokens = len(split)
+
                 # range_results = MpolynomDocument.search().filter('range', nb_tokens ={'lte': nb_tokens+2, 'gte': nb_tokens-2})
-                match_results =  MpolynomDocument.search().query("multi_match", query = b, fields = ['mpolynomyal^3',
-                'structure_name^3','keywords^2','comments','references','links','author^2'],fuzziness = "AUTO")
+                #match_results =  MpolynomDocument.search().query("multi_match", query = b, fields = ['mpolynomyal^3',
+                #'structure_name^3','keywords^2','comments','references','links','author^2'],fuzziness = "AUTO")
 
                 # search for mpoly with the same length
-                q0 = Q('bool',
-                must=[Q("multi_match", query = b, fields = ['mpolynomyal^3','structure_name^3','keywords^2',
-                'comments','references','links','author^2'],fuzziness = "AUTO:4,7", minimum_should_match = '85%'  #, collapse = {"field" : "nb_tokens"} , sort = [{"nb_tokens" : {"order" : "asc", "mode" : "median"}}]
-                ),
-                Q('range',  nb_tokens = {'lte': nb_tokens, 'gte': nb_tokens})
-                ]) 
-                q1 = Q('bool',
-                must=[Q("multi_match", query = b, fields = ['mpolynomyal^3','structure_name^3','keywords^2',
-                'comments','references','links','author^2'],fuzziness = "AUTO:4,7", minimum_should_match = '85%'  #, collapse = {"field" : "nb_tokens"} , sort = [{"nb_tokens" : {"order" : "asc", "mode" : "median"}}]
-                ),
-                Q('range',  nb_tokens = {'lte': nb_tokens+2, 'gte': nb_tokens+1})
-                ])
-                q2 = Q('bool',
-                must=[Q("multi_match", query = b, fields = ['mpolynomyal^3','structure_name^3','keywords^2',
-                'comments','references','links','author^2'],fuzziness = "AUTO:4,7", minimum_should_match = '85%'  #, collapse = {"field" : "nb_tokens"} , sort = [{"nb_tokens" : {"order" : "asc", "mode" : "median"}}]
-                ),
-                Q('range',  nb_tokens = {'lte': nb_tokens-1, 'gte': nb_tokens-2})
-                ])
+                response0 = mpoly_query(b, nb_tokens, nb_tokens)
 
-                #def query(lte, gte): # lahko spreminjaš +2 -2 glede na dolžino besede - delež
-                # spremeni v funkcijo, da se ne ponavljaš
-                # q0 = Q('bool',
-                # must=[Q("multi_match", query = b, fields = ['mpolynomyal^3','structure_name^3','keywords^2',
-                # 'comments','references','links','author^2'],fuzziness = "AUTO"  #, collapse = {"field" : "nb_tokens"} , sort = [{"nb_tokens" : {"order" : "asc", "mode" : "median"}}]
-                # ),
-                # Q('range',  nb_tokens = {'lte': lte, 'gte': gte})
-                # ]) 
+                # search for mpoly with 1 part (člen) more than original mpoly
+                response1 = mpoly_query(b, nb_tokens+4, nb_tokens+1)
+                # search for mpoly with 1 part less than original mpoly
+                response2 = mpoly_query(b, nb_tokens-1, nb_tokens-4)
 
-                results0 = MpolynomDocument.search().query(q0) 
-                response0 = results0.execute()
                 number_results0 = response0.hits.total.value
                 if number_results0 > 10:
                     response0 = response0[0:10]
 
-                results1 = MpolynomDocument.search().query(q1) 
-                response1 = results1.execute()
                 number_results1 = response1.hits.total.value
                 if number_results1 > 10:
                     response1 = response1[0:10]
 
-                results2 = MpolynomDocument.search().query(q2) 
-                response2 = results2.execute()
                 number_results2 = response2.hits.total.value
                 if number_results2 > 10:
                     response2 = response2[0:10]
